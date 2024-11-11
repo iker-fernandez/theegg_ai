8f671c98e8b84d631c8ace5c8e08afbc6c8d4aa6
iker-fernandez <ikerirura@gmail.com>
2024-11-11 11:39:45 +0100

    Create tiempo.py

---
+++ tiempo.py
@@ -0,0 +1,10 @@
+#♥Modulo que calcula la altura desde la que cae un objeto sabiendo el tiempo que tarda en caer
+def tierra(tiempo):
+  g=9.8
+  return 0.5*g*tiempo**2
+def marte(tiempo):
+  g=3.7
+  return 0.5*g*tiempo**2
+def jupiter(tiempo):
+  g=24.8
+  return 0.5*g*tiempo**2
